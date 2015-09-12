287f289e11896efaa4bc85490d4c57006d5605d3
Jonas Almeida <jalmeida@mathbiol.org>
2015-09-12 12:06:33 -0400

    weekend break

--- imagebox.js
+++ imagebox.js
@@ -2,21 +2,17 @@ console.log('imagebox.js loaded')
 
 imagebox=function(){
     // ini
-    imagebox.tests()
 }
 
 imagebox.msg=function(x){console.log(x)}
-imagebox.serviceUrl='http://130.245.124.21:9090/imagebox'
-imagebox.imageUrl='http://130.245.124.21:9090/imagebox'
-imagebox.tests=function(){
-    // some early testing of Erich's server
-    this.msg('running some tests ...')
-    var box = 'http://130.245.124.21:9090/imagebox'
-    var url='https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/gbm/bcr/nationwidechildrens.org/tissue_images/slide_images/nationwidechildrens.org_GBM.tissue_images.Level_1.1.42.0/TCGA-02-0001-01C-01-BS1.0cc8ca55-d024-440c-a4f0-01cf5b3af861.svs'
-    var xywh=[3000,8000,1058,1058]
-
-
+imagebox.box='http://130.245.124.21:9090/imagebox'
+imagebox.get=function(url,img){
+    if(!img){img = document.createElement('img')}
+    if(!url){url='http://130.245.124.21:9090/data/tcga_data/tumor/blca/bcr/nationwidechildrens.org/tissue_images/slide_images/nationwidechildrens.org_BLCA.tissue_images.Level_1.113.1.0/TCGA-BL-A13I-01A-01-BSA.svs?xywh=5500,16000,1000,1000'}
+    img.src=url
+    return img
 }
+
 
 
 imagebox()
