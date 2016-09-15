ffbc0d0c73a1582106c8f5559619ed1ed69a15f2
Jonas Almeida <jalmeida@mathbiol.org>
2016-09-15 16:55:20 -0400

    default focus on [8300,19000,1000,1000]

--- imagebox.js
+++ imagebox.js
@@ -5,7 +5,7 @@ imagebox=function(){
     if(location.hash.length<2){
         //location.hash='http://130.245.124.21:9090/imagebox?url=https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/gbm/bcr/nationwidechildrens.org/tissue_images/slide_images/nationwidechildrens.org_GBM.tissue_images.Level_1.1.42.0/TCGA-02-0001-01C-01-BS1.0cc8ca55-d024-440c-a4f0-01cf5b3af861.svs&xywh=10000,8000,1000,1000'
         //location.hash='http://imagebox-128854.nitrousapp.com:3000/imagebox?url=https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/gbm/bcr/nationwidechildrens.org/tissue_images/slide_images/nationwidechildrens.org_GBM.tissue_images.Level_1.1.42.0/TCGA-02-0001-01C-01-BS1.0cc8ca55-d024-440c-a4f0-01cf5b3af861.svs&xywh=10000,8000,1000,1000&scale=0'
-        location.hash='http://imagebox-128854.nitrousapp.com:3000/imagebox?url=http://openslide.cs.cmu.edu/download/openslide-testdata/Aperio/CMU-1.svs&xywh=9300,19000,1000,1000&scale=0'
+        location.hash='http://imagebox-128854.nitrousapp.com:3000/imagebox?url=http://openslide.cs.cmu.edu/download/openslide-testdata/Aperio/CMU-1.svs&xywh=8300,19000,1000,1000&scale=0'
         imagebox.msg('no image target found so the default is being used')
     }
     //sliceURL.innerHTML=""
